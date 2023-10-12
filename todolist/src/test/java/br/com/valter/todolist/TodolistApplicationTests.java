4ce8891aaa266a8028348e8aeafeedacff7d7f81
ValterMonteiro <valtinms@gmail.com>
2023-10-12 14:15:23 -0400

    iniciando projeto

---
+++ todolist/src/test/java/br/com/valter/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valter.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
